770725b2757e5ca7b50fe148b5a8f13a43b10818
patrick96 <p.ziegler96@gmail.com>
2016-05-03 23:33:10 +0200

    Corrected the repo URL. Deleted not needed files

--- spec/click-link-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import ClickLinkView from '../lib/click-link-view';
-
-describe('ClickLinkView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
